4e750fcfea6a80214455ca331c92d13069ff56ce
Niklas Mollenhauer <nikeee@outlook.com>
2023-12-08 18:34:50 +0100

    Add basic api sketch

---
+++ src/SplidClient2.ts
@@ -0,0 +1,54 @@
+import Parse from 'parse/node';
+import { CurrencyCode, DeviceType } from './types/primitives';
+
+// Sadly, the parse JS SDK is a singleton, so this config is global
+Parse.initialize(
+  'AKCaB0FCF0NIigWjxcDBpDYh7q6eN7gYfKxk5QBN',
+  '4Z29DJvRGdVnB5dcTvDTTG01fbkITxvcPCPOt21M'
+);
+Parse.serverURL = 'https://splid.herokuapp.com/parse';
+
+export interface GroupInfo {
+  shortCode: string;
+  longCode: string;
+  extendedShortCode: string;
+  objectId: string;
+}
+
+export type GroupCodes = Omit<GroupInfo, 'objectId'>;
+
+export interface SplidClient {
+  createGroup(): Promise<GroupInfo>;
+
+  fetchGroupCodes(groupObjectId: string): Promise<GroupCodes>;
+}
+
+export class ParseSplidClient implements SplidClient {
+  createGroup(): Promise<GroupInfo> {
+    return Parse.Cloud.run('createGroup');
+  }
+
+  fetchGroupCodes(groupObjectId: string): Promise<GroupCodes> {
+    return Parse.Cloud.run('fetchCodes', {
+      group: groupObjectId,
+    });
+  }
+
+  joinGroupWithAnyCode(code: string): Promise<GroupInfo> {
+    return Parse.Cloud.run('joinGroupWithAnyCode', { code });
+  }
+
+  getCurrencyRates(): Promise<Record<CurrencyCode, number>> {
+    return Parse.Cloud.run('getCurrencyRates');
+  }
+
+  getCodeConfig(deviceType: DeviceType): Promise<string> {
+    return Parse.Cloud.run('getCodeConfig', { deviceType });
+  }
+}
+
+// export class SplidBatchClient implements SplidClient {
+//   createGroup(): Promise<GroupInfo> {
+//     throw new Error('Method not implemented.');
+//   }
+// }
--- src/types/primitives.ts
+++ src/types/primitives.ts
@@ -1,22 +1,26 @@
 /**
- * e.g. "9c261483-2c88-4685-aae2-34531aedaf2a"
- *
  * Can be created using `crypto.randomUUID()`
+ *
+ * @example "9c261483-2c88-4685-aae2-34531aedaf2a"
  */
-
 export type Uuid = string;
+
 /**
- * e.g. "2023-08-22T15:27:23.403Z"
+ * Date time in ISO 8601 format.
+ *
+ * @example "2023-08-22T15:27:23.403Z"
  */
 export type IsoTime = string;
 
 /**
- * e.g. "EUR"
+ * ISO 4217 currency code.
+ *
+ * @example "EUR"
  */
 export type CurrencyCode = string;
 
 /**
- * e.g. 0.0.1, 1.2.1
+ * @example "0.0.1", "1.2.1"
  */
 export type Version = `${number}.${number}.${number}`;
 
@@ -24,3 +28,5 @@ export type UserId = string & { readonly brand?: unique symbol };
 export type GroupId = string & { readonly brand?: unique symbol };
 export type EntryId = string & { readonly brand?: unique symbol };
 export type InstallationId = string & { readonly brand?: unique symbol };
+
+export type DeviceType = 'ios' | 'android';
