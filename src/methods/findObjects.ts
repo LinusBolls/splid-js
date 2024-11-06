8cfa6ffefb8fe651304cfaf91275cf87fa008cad
Linus Bolls <linus.bolls@gmail.com>
2024-11-06 15:56:50 +0100

    refactor: remove unnecessary dateToIso implementation

--- src/methods/findObjects.ts
+++ src/methods/findObjects.ts
@@ -12,18 +12,6 @@ export type FindObjectsResponse<T extends unknown> = {
   };
 };
 
-const dateToIso = (date: Date) => {
-  const year = date.getUTCFullYear();
-  const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Months are 0-indexed, so +1 is necessary
-  const day = String(date.getUTCDate()).padStart(2, '0');
-  const hours = String(date.getUTCHours()).padStart(2, '0');
-  const minutes = String(date.getUTCMinutes()).padStart(2, '0');
-  const seconds = String(date.getUTCSeconds()).padStart(2, '0');
-  const milliseconds = String(date.getUTCMilliseconds()).padStart(3, '0');
-
-  return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}Z`;
-};
-
 export const findObjects =
   <T extends keyof ClassName>(className: T) =>
   async (
@@ -39,7 +27,7 @@ export const findObjects =
       className,
       minDate: {
         __type: 'Date',
-        iso: minDate ? dateToIso(minDate) : '1969-12-31T00:00:00.000Z',
+        iso: minDate?.toISOString() ?? '1969-12-31T00:00:00.000Z',
       },
       group: groupId,
       limit,
